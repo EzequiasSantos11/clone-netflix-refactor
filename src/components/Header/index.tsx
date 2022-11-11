4cf2d03c3690111472a6ba1dc31f5b4ba8740536
ezequiassantos11 <ezequiasmg16@gmail.com>
2022-11-10 22:04:36 -0300

    bugrefects

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from 'react';
 import { Container } from './styles';
 
@@ -10,14 +11,18 @@ export function Header({ black }: HeaderProps){
  return(
   <Container className={black ? 'black' : ''}>
    <div className="logo">
-    <a href="/">
-     <img src="./img/netflix-logo.svg" alt="logo" />
-    </a>
+    <Link href="/">
+      <a>
+        <img src="./img/netflix-logo.svg" alt="logo" />
+      </a>
+    </Link>
    </div> 
    <div className="user">
-    <a href="/users">
-     <img src="https://github.com/ezequiassantos11.png" alt="user" />
-    </a>
+    <Link href="/">
+      <a>
+      <img src="https://github.com/ezequiassantos11.png" alt="user" />
+      </a>
+    </Link>
    </div>
   </Container>
  );
